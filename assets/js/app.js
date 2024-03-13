8c2d57c547acdfead42f483329c3cfa63f903913
alessiavozzo <a.vozzo95@gmail.com>
2024-03-13 15:11:28 +0100

    add toDoTasks and add milestone 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,42 @@
-console.log("ok");
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            
+            toDoTasks:[
+                {
+                    text: "fare la spesa",
+                    done: false
+                },
+                {
+                    text: "fare l'esercizio di Boolean",
+                    done: false
+                },
+                {
+                    text: "portare fuori il cane",
+                    done: true
+                },
+                {
+                    text: "chiamare il geometra",
+                    done: true
+                },
+                {
+                    text: "portare fuori la spazzatura",
+                    done: false
+                },
+                {
+                    text: "preparare la cena",
+                    done: true
+                },
+                {
+                    text: "comprare delle penne che scrivono",
+                    done: false
+                },
+            ]
+
+
+        }
+    }
+}).mount('#app')
